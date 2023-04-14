73d5f3446f84129347a88bea8e06b5d5b8b42da0
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-14 22:11:01 +0200

    Create Heder component and add styles

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,76 @@
+<template>
+  <header>
+    <nav>
+      <h1>
+        <router-link to="/" class="logo-link">Find your Coach!</router-link>
+      </h1>
+      <ul>
+        <li><router-link to="/coaches" class="list-link">All coaches</router-link></li>
+        <li><router-link to="/requests"  class="list-link">Requests</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header a {
+  text-decoration: none;
+  color: white;
+  display: inline-block;
+  padding: 0.75rem 1.5rem;
+  border: 1px solid transparent;
+}
+
+.list-link:active,
+.list-link:hover,
+.list-link.router-link-active {
+  border: 1px solid white;
+  background-color: white;
+  color: blue;
+}
+
+h1 {
+  color: white;
+  margin: 0;
+}
+
+h1 a {
+  color: white;
+  margin: 0;
+}
+
+.logo-link:hover,
+.logo-link:active{
+  border-color: transparent;
+}
+
+header nav {
+  width: 90%;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+header ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 0.5rem;
+}
+</style>
