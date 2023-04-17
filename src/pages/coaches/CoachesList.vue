7f22c84c8ea68aefc21edda137b80dc94d00dc4d
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-17 13:50:50 +0200

    Add a list of coaches and render coaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>
-        Filter
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">register as a coach</router-link>
     </div>
+    <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName }}</li>
+    </ul>
+    <h3 v-else>No coaches found :(</h3>
+  </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+        return this.$store.getters['coaches/hasCoaches']
+    }
+  },
+};
+</script>
