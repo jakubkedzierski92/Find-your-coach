6ddab713b58e08e9bef3adb1cd16e1d44d896daf
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-17 13:50:13 +0200

    Fix the issue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <router-view>
-    <the-header></the-header>
-  </router-view>
+  <the-header></the-header>
+  <router-view> </router-view>
 </template>
 
 <script>
 import TheHeader from './components/layout/TheHeader.vue';
 
 export default {
-    components: {
-        TheHeader
-    }
-}
+  components: {
+    TheHeader,
+  },
+};
 </script>
 
 <style>
