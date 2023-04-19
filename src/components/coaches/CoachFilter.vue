1db9f6e23366b13c3080114510dfc4dde29f4b2b
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-19 13:27:05 +0200

    Add filter through coaches and coach registration form

--- src/components/coaches/CoachFilter.vue
+++ src/components/coaches/CoachFilter.vue
@@ -1,6 +1,6 @@
 <template>
     <base-card>
-      <h2>I'm looiking for ...</h2>
+      <h2>I'm looking for ...</h2>
       <span class="filter-option">
         <input type="checkbox" id="frontend" checked @change="setFilter" />
         <label for="frontend">Frontend</label>
