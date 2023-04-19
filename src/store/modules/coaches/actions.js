94a0d6d4bcf81f74eafebcf7e38b4c9a57832363
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-19 16:47:30 +0200

    Add fetching data from firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -18,13 +18,36 @@ export default {
     );
 
     //const responseData = await response.json()
-    if(!response.ok) {
-      //error 
+    if (!response.ok) {
+      //error
     }
 
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
+      id: userId,
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://findyourcoach-b1af5-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+    if (!response.ok) {
+      // console.error();
+    }
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach)
+    }
+    context.commit('setCoaches', coaches)
   },
 };
