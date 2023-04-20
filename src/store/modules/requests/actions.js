9f304c246e771e606e12de7ebc046f6f6be96aac
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-20 11:46:20 +0200

    Requests store

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -1,11 +1,50 @@
-export default{
-    contactCoach(context, payload) {
-        const newRequest = {
-            id: new Date().toISOString(),
-            coachId: payload.coachId,
-            userEmail: payload.email,
-            message: payload.message
-        }
-        context.commit('addRequest', newRequest)
+export default {
+  async contactCoach(context, payload) {
+    const newRequest = {
+      userEmail: payload.email,
+      message: payload.message,
+    };
+    const response = await fetch(
+      `https://findyourcoach-b1af5-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`,
+      {
+        method: 'POST',
+        body: JSON.stringify(newRequest),
+      }
+    );
+
+    const responseData = await response.json()
+
+    if(!response.ok) {
+        const error = new Error(responseData.message || " something went wrong...")
+        throw error
     }
-}
+    newRequest.id = responseData.name
+    newRequest.coachId = payload.coachId
+
+    context.commit('addRequest', newRequest);
+  },
+
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId
+    const response = await fetch(`https://findyourcoach-b1af5-default-rtdb.firebaseio.com/requests/${coachId}.json`,)
+    const responseData = await response.json()
+
+    if(!response.ok) {
+        const error = new Error(responseData.message || " something went wrong...")
+        throw error
+    }
+    const requests = [];
+
+    for (const key in responseData) {
+        const request = {
+            id: key,
+            coachId: coachId,
+            userEmail: responseData[key].userEmail,
+            message: responseData[key].message
+        }
+        requests.push(request)
+    }
+    context.commit('setRequest', requests)
+  }
+
+};
