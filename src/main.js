d1b17f30649ad1e6af7d6e737585c5b3e4756eb4
Jakub Kedzierski <kedzierski.jakub92@gmail.com>
2023-04-27 19:41:42 +0200

    Optimize components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 import App from './App.vue';
 import router from './router.js';
 import store from './store/index.js';
@@ -6,7 +6,9 @@ import BaseCard from './components/base/BaseCard.vue'
 import BaseButton from './components/base/BaseButton.vue'
 import BaseBadge from './components/base/BaseBadge.vue'
 import BaseSpinner from './components/base/BaseSpinner.vue'
-import BaseDialog from './components/base/BaseDialog.vue'
+// import BaseDialog from './components/base/BaseDialog.vue'
+
+const BaseDialog = defineAsyncComponent(() => import('./components/base/BaseDialog.vue'))
 
 const app = createApp(App);
 app.component('base-card', BaseCard)
